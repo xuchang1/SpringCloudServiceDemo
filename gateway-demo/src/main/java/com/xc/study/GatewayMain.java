c753a5b657912ce10dbc176c56f305c0a740b5c8
xuchang1 <1263919792@qq.com>
2021-08-11 14:39:21 +0800

    gateway

---
+++ gateway-demo/src/main/java/com/xc/study/GatewayMain.java
@@ -0,0 +1,16 @@
+package com.xc.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author changxu13
+ * @date 2021/8/11 12:40
+ */
+@SpringBootApplication
+public class GatewayMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMain.class, args);
+	}
+}
